77d6d97ac9ce2084156205b3be1a9e41ef46651c
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2021-12-21 17:29:29 -0300

    Fix para exibir o mes corretamente.

--- capitulo-2/hello_world/src/main.rs
+++ capitulo-2/hello_world/src/main.rs
@@ -3,6 +3,8 @@ extern crate time;
 fn main() {
     let	d = time::now();
 	println!("Today is {}/{}/{}", d.tm_mday,
-	        d.tm_mon, d.tm_year + 1900);
+	        d.tm_mon + 1, d.tm_year + 1900); //fix do mês
 
+    //https://docs.rs/time/0.1.37/time/struct.Tm.html
+    //tm_mon: i32 - Months since January - [0, 11]
 }
